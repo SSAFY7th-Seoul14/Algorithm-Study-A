821d016f9a81a59d4106751b51a7ad4286099d4e
김지건 <jigun21@naver.com>
2022-02-08 19:51:03 +0900

    [BOJ] 1925 히스토그램 / P5

--- patata22/BOJ_1935.java
+++ patata22/BOJ_1935.java
@@ -1,10 +1,8 @@
-import java.sql.SQLOutput;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Stack;
 
-public class Main {
+public class BOJ_1935 {
     static String temp = "+-*/";
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
