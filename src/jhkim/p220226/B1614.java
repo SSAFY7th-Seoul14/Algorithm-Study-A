c7144de9683d602bbb1db5dcef147a1a5416f1eb
jh9706103 <jh9706103@gmail.com>
2022-02-26 22:05:44 +0900

    [BOJ] 1614 영식이의 손가락 / S2

---
+++ src/jhkim/p220226/B1614.java
@@ -0,0 +1,38 @@
+package jhkim.p220226;
+
+import java.util.Scanner;
+
+public class B1614 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int injured = sc.nextInt();
+		long times = sc.nextLong();
+		long result = 0;
+
+		if (injured != 1 && injured != 5) {
+			if (times == 0) {
+				result = injured - 1;
+			} else if (times % 2 == 1) {
+				result = (times / 2) * 8 + injured + (5 - injured) * 2 - 1;
+			} else if (times % 2 == 0) {
+				result = (times / 2) * 8 + injured - 1;
+			}
+		} else if (injured == 1) {
+			if (times == 0) {
+				result = injured - 1;
+			} else {
+				result = times * 8;
+			}
+		} else if (injured == 5) {
+			if (times == 0) {
+				result = injured - 1;
+			} else {
+				result = times * 8 + 4;
+			}
+		}
+		System.out.println(result);
+	}
+}
